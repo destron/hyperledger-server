caa6db744b396b21e325c5d2ad4cb6e1c2458ac8
Daniel Feichtinger <df@hyperledger.org>
2015-06-05 16:52:44 +0800

    Break out setup fns for prepare controller

--- test/web/controllers/prepare_controller_test.exs
+++ test/web/controllers/prepare_controller_test.exs
@@ -5,15 +5,20 @@ defmodule Hyperledger.PrepareControllerTest do
   alias Hyperledger.SecretStore
   alias Hyperledger.PrepareConfirmation, as: Prepare
   
-  test "accept valid prepare" do
+  setup do
     {primary, secret} = create_node_with_secret(1)
     {replica, _secret} = create_node_with_secret(2)
     System.put_env("NODE_URL", replica.url)
+    
     {:ok, secret_store} = SecretStore.start_link
     client_params = asset_params("{}", secret_store)
     public_key = client_params[:asset][:publicKey]
     params = prepare_params(1, 1, "asset/create", client_params, public_key, secret_store)
     
+    {:ok, params: params, primary: primary, secret: secret}
+  end
+  
+  test "accept valid prepare", %{params: params, primary: primary, secret: secret} do
     conn = post_authentic_json("/prepare", params, {primary.public_key, secret})
     
     assert conn.status == 201
